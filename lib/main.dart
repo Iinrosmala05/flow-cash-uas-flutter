2c54bbaa1dd211cbea79f0fc828a5628fc705ed2
Iinrosmala05 <rosmala01011991@gmail.com>
2026-05-06 07:43:09 +0700

    Update: Berhasil memperbaiki logika hitung saldo dan pembersihan input angka

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(const FlowCashApp());
 
@@ -15,8 +16,29 @@ class FlowCashApp extends StatelessWidget {
   }
 }
 
-class DashboardPremium extends StatelessWidget{
+class DashboardPremium extends StatefulWidget {
   const DashboardPremium({super.key});
+
+  @override
+  State<DashboardPremium> createState() => _DashboardPremiumState();
+}
+
+class _DashboardPremiumState extends State<DashboardPremium> {
+  int totalSaldo = 10500000;
+  List<Map<String, dynamic>> transactions = [
+    {
+      "title": "Makan Siang",
+      "amount": "Rp 25.000",
+      "icon": Icons.fastfood,
+      "color": Colors.orange,
+    },
+    {
+      "title": "Beli Bensin",
+      "amount": "Rp 50.000",
+      "icon": Icons.directions_car,
+      "color": Colors.blue,
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -30,140 +52,154 @@ class DashboardPremium extends StatelessWidget{
           ),
         ),
         child: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Padding(
-                padding: EdgeInsets.all(25.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Selamat Datang", style: TextStyle(color: Colors.white, fontSize: 16)),
-                        Text("Iin Rosmala", style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold)),
-                      ],
-                    ),
-                    CircleAvatar(
-                      radius: 25,
-                      backgroundColor: Colors.white24,
-                      child: Icon(Icons.person, color: Colors.white),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 25),
-                height: 200,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(25),
-                  gradient: LinearGradient(
-                    colors: [Colors.white.withOpacity(0.2), Colors.white.withOpacity(0.05)],
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                  ),
-                  border: Border.all(color: Colors.white.withOpacity(0.2)),
-                ),
-                child: Stack(
-                  children: [
-                    Positioned(
-                      top: 20,
-                      right: 20,
-                      child: Icon(Icons.contactless,
-                      color: Colors.white.withOpacity(0.5), size: 40),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(25.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          const Text("TOTAL SALDO", style: TextStyle(color: Colors.white70, letterSpacing: 2)),
-                          const SizedBox(height: 5),
-                          Row(
-                            children: [
-                              _infoTab("Masuk", "Rp 5jt"),
-                              const SizedBox(width: 30),
-                              _infoTab("Keluar", "Rp 1.2jt"),
-                            ],
-                          )
-                        ],
-                      ), 
-                    )
-                  ],
-                ),
-              ),
-              const SizedBox(height: 30),
-              Expanded(
-                child: Container(
-                  padding: const EdgeInsets.only(top: 30, left: 25, right: 25 ),
-                  decoration: const BoxDecoration(
-                    color: Colors.white, borderRadius: BorderRadius.only(topLeft: Radius.circular(40), topRight: Radius.circular(40)),
-                  ),
-                  child: Column(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          const Text("Transaksi Terakhir", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                          Text("Lihat Semua", style: TextStyle(color: Colors.blue[900], fontWeight: FontWeight.w600)),
+                          Text(
+                            "Selamat Datang",
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
+                          Text(
+                            "Iin Rosmala",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
                         ],
                       ),
-                      const SizedBox(height: 20),
-                      Expanded(
-                        child: ListView(
-                          children: [
-                            _transactionItem("Makan Siang", "27 Apr", "Rp 50.000", Icons.fastfood, Colors.orange),
-                            _transactionItem("Gaji Project", "25 Apr", "+ Rp 2.000.000", Icons.work, Colors.green),
-                            _transactionItem("Nonton Bioskop", "21 Apr", "Rp 45.000", Icons.movie, Colors.red),
-                          ],
-                        ),
-                      )
+                      const CircleAvatar(
+                        radius: 25,
+                        backgroundColor: Colors.white24,
+                        child: Icon(Icons.person, color: Colors.white),
+                      ),
                     ],
                   ),
                 ),
-              )
-            ],
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 25),
+                  padding: const EdgeInsets.all(25),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    gradient: const LinearGradient(
+                      colors: [Color(0xFF00B4DB), Color(0xFF0083B0)],
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children:[
+                      Text(
+                        "Rp ${totalSaldo.toString().replaceAllMapped(RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'),(Match m) => '${m[1]}.')}",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 30),
+
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: Text(
+                    "Riwayat Transaksi",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: transactions.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        color: Colors.white.withOpacity(0.05),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        margin: const EdgeInsets.symmetric(vertical: 8),
+                        child: ListTile(
+                          leading: CircleAvatar(
+                            backgroundColor: transactions[index]['color']
+                                .withOpacity(0.2),
+                            child: Icon(
+                              transactions[index]['icon'],
+                              color: transactions[index]['color'],
+                            ),
+                          ),
+                          title: Text(
+                            transactions[index]['title'],
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          subtitle: const Text(
+                            "Hari Ini",
+                            style: TextStyle(color: Colors.white54),
+                          ),
+                          trailing: Text(
+                            transactions[index]['amount'],
+                            style: const TextStyle(
+                              color: Colors.redAccent,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                const SizedBox(height: 100),
+              ],
+            ),
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) => const AddTransactionPage()),
+        onPressed: () async {
+          final result = await Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const AddTransactionPage()),
           );
-        },
+          if (result != null) {
+            setState(() {
+              transactions.insert(0, result);
+
+              String nominalString = result['amount'].toString();
+              String hanyaAngka= nominalString.replaceAll(RegExp(r'[^[0-9]'),'');
+              int angkaTransaksi = int.tryParse(hanyaAngka) ?? 0;
+              totalSaldo = totalSaldo - angkaTransaksi;
+            });
+          }
+        },                                                                         
         backgroundColor: const Color(0xFF2C5364),
         child: const Icon(Icons.add, color: Colors.white),
       ),
     );
   }
 
-  Widget _infoTab(String label,String value) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(label, style: const TextStyle(color: Colors.white60, fontSize: 12)),
-        Text(value, style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
-      ],
-    );
-  }
-
-  Widget _transactionItem(String title, String date, String amount, IconData icon, Color color) {
-    return ListTile(
-      contentPadding: EdgeInsets.zero,
-      leading: Container(
-        padding: const EdgeInsets.all(10),
-        decoration: BoxDecoration(color: color.withOpacity(0.1),
-        borderRadius: BorderRadius.circular(15)),
-        child: Icon(icon, color: color),
-      ),
-      title: Text(title, style: const TextStyle(fontWeight: FontWeight.bold)),
-      subtitle: Text(date),
-      trailing: Text(amount, style: TextStyle(fontWeight: FontWeight.bold, color: amount.contains('+') ? Colors.green : Colors.black)),
-    );
-  }
 }
 
 class AddTransactionPage extends StatefulWidget {
@@ -175,12 +211,16 @@ class AddTransactionPage extends StatefulWidget {
 
 class _AddTransactionPageState extends State<AddTransactionPage> {
   String selectedCategory = "";
+  final TextEditingController _amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Tambah Transaksi", style: TextStyle(color: Colors.white)),
+        title: const Text(
+          "Tambah Transaksi",
+          style: TextStyle(color: Colors.white),
+        ),
         backgroundColor: const Color(0xFF0F2027),
         iconTheme: const IconThemeData(color: Colors.white),
       ),
@@ -196,15 +236,27 @@ class _AddTransactionPageState extends State<AddTransactionPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text("Nominal Pengeluaran", style: TextStyle(color: Colors.white70)),
-            const TextField(
-              autofocus: true,
+            const Text(
+              "Nominal Pengeluaran",
+              style: TextStyle(color: Colors.white70),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: _amountController,
               keyboardType: TextInputType.number,
-              style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),
-              decoration: InputDecoration(
-                hintText: "Rp 0",
-                hintStyle: TextStyle(color: Colors.white24),
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 45,
+                fontWeight: FontWeight.bold,
+              ),
+              cursorColor: Colors.white,
+              decoration: const InputDecoration(
+                prefixText: "Rp ",
+                prefixStyle: TextStyle(color: Colors.white, fontSize: 45),
                 border: InputBorder.none,
+                hintText: "0",
+                hintStyle: TextStyle(color: Colors.white24),
               ),
             ),
             const SizedBox(height: 30),
@@ -226,31 +278,57 @@ class _AddTransactionPageState extends State<AddTransactionPage> {
                 style: ElevatedButton.styleFrom(
                   backgroundColor: const Color(0xFF2C5364),
                   padding: const EdgeInsets.symmetric(vertical: 15),
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadiusGeometry.circular(15)),
-                ),
-                onPressed:() { Navigator.pop(context);
-
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text("Mantap! Transaksi $selectedCategory berhasil dicatat"),
-                    backgroundColor: Colors.green,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
                   ),
-                );
-              }, 
-              child: const Text("SIMPAN TRANSAKSI", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+                ),
+                onPressed: () {
+                  String isiNominal = _amountController.text;
+
+                  if (isiNominal.isEmpty || selectedCategory.isEmpty) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text(
+                          "Isi nominal dan pilih kategori dulu ya!",
+                        ),
+                      ),
+                    );
+                  } else {
+                    Navigator.pop(context, {
+                      "title": selectedCategory,
+                      "amount": "Rp ${_amountController.text}",
+                      "icon": selectedCategory == "makan" ? Icons.fastfood: (selectedCategory == "Transport" ?Icons.directions_car: Icons.shopping_bag),
+                      "color": selectedCategory== "makan" ? Colors.orange: (selectedCategory == "Transport" ? Colors.blue: Colors.pink),
+                    });
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text("Mantap! Transaksi $selectedCategory berhasil dicatat"),
+                        backgroundColor: Colors.green,
+                      ),
+                    ); 
+                  }
+                },
+                child: const Text(
+                  "SIMPAN TRANSAKSI",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
-    ),
-  );
-}
-Widget _categoryIcon (IconData icon, String label) {
+    );
+  }
+
+  Widget _categoryIcon(IconData icon, String label) {
     bool isSelected = selectedCategory == label;
 
     return GestureDetector(
       onTap: () {
-        setState((){
+        setState(() {
           selectedCategory = label;
         });
       },
@@ -259,24 +337,26 @@ Widget _categoryIcon (IconData icon, String label) {
           Container(
             padding: const EdgeInsets.all(15),
             decoration: BoxDecoration(
-            color: isSelected ? Colors.blueAccent : Colors.white.withOpacity(0.1),
-            borderRadius: BorderRadius.circular(15),
-            border: isSelected ? Border.all(color: Colors.white, width: 2) : null,
+              color: isSelected
+                  ? Colors.blueAccent
+                  : Colors.white.withOpacity(0.1),
+              borderRadius: BorderRadius.circular(15),
+              border: isSelected
+                  ? Border.all(color: Colors.white, width: 2)
+                  : null,
             ),
-          child: Icon(icon, color: Colors.white),
+            child: Icon(icon, color: Colors.white),
           ),
           const SizedBox(height: 8),
           Text(
             label,
             style: TextStyle(
               color: isSelected ? Colors.white : Colors.white60,
-              fontWeight: isSelected ? FontWeight.bold : FontWeight.normal
-            )
+              fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+            ),
           ),
         ],
       ),
     );
   }
 }
-
- 
